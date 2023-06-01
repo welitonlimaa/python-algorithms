ca6b887d7748a4ad5f62b6e2dd28cab4ca9184fe
welitonlimaa <welitonlima159@gmail.com>
2023-06-01 19:57:51 -0300

    requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,22 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    if (
+        len(word) == 1 or
+        low_index > high_index
+    ):
+        return True
+    if word[low_index] != word[high_index]:
+        return False
+    if (
+        low_index <= high_index
+    ):
+        return is_palindrome_recursive(word, (low_index + 1), (high_index - 1))
+
+    return False
+
+
+str_word = "AGUA"
+index = len(str_word) - 1
+
+print(is_palindrome_recursive(str_word, 0, index))
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,25 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+# import pytest
+
+from challenges.challenge_encrypt_message import encrypt_message
 
 
+# @pytest.mark.parametrize(
+#     "message, key, expected",
+#     [
+#         ("aloalo", 3, "ola_ola"),
+#         ("aloalo", "B", "tipo inválido para key"),
+#         (6594, 3, "tipo inválido para message"),
+#         ("olaola", 9, "ola_ola"),
+#     ],
+# )
 def test_encrypt_message():
-    pass
+    assert encrypt_message("aloalo", 3) == "ola_ola"
+    assert encrypt_message("aloalo", 10) == "olaola"
+    try:
+        encrypt_message("aloalo", "B")
+    except TypeError as e:
+        print("error", str(e) == "tipo inválido para key")
+        assert str(e) == "tipo inválido para key"
+
+# def test_encrypt_message():
+#     assert encrypt_message("aloalo", 3) == "nnaja"
