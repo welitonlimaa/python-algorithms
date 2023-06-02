81a93fd018c1c03f4aad6b9eebb3281e6a1b0ca3
welitonlimaa <welitonlima159@gmail.com>
2023-06-02 17:16:35 -0300

    requisito 2 finalizado

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,25 +1,14 @@
-# import pytest
+import pytest
 
 from challenges.challenge_encrypt_message import encrypt_message
 
 
-# @pytest.mark.parametrize(
-#     "message, key, expected",
-#     [
-#         ("aloalo", 3, "ola_ola"),
-#         ("aloalo", "B", "tipo inválido para key"),
-#         (6594, 3, "tipo inválido para message"),
-#         ("olaola", 9, "ola_ola"),
-#     ],
-# )
 def test_encrypt_message():
     assert encrypt_message("aloalo", 3) == "ola_ola"
+    assert encrypt_message("aloalo", 4) == "ol_aola"
     assert encrypt_message("aloalo", 10) == "olaola"
-    try:
-        encrypt_message("aloalo", "B")
-    except TypeError as e:
-        print("error", str(e) == "tipo inválido para key")
-        assert str(e) == "tipo inválido para key"
 
-# def test_encrypt_message():
-#     assert encrypt_message("aloalo", 3) == "nnaja"
+    with pytest.raises(TypeError):
+        encrypt_message("aloalo", "B")
+    with pytest.raises(TypeError):
+        encrypt_message(12354, 3)
